71573dcf89abf86dbddb890716880375672a9843
Berkeley Churchill <berkeley@cs.stanford.edu>
2019-04-16 19:10:39 +0000

    fixing username for docker image

--- scripts/docker/user-setup.sh
+++ scripts/docker/user-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /home/stoke/stoke
+cd /home/equivalence/equivalence-checker
 
 rm -rf src/ext/z3
 
 # get links to previously compiled CVC4 / Z3
-ln -s /home/stoke/base/src/ext/z3 src/ext/z3
-ln -s /home/stoke/base/src/ext/cvc4-1.5-build src/ext/cvc4-1.5-build
-ln -s /home/stoke/base/src/ext/cvc4-1.5 src/ext/cvc4-1.5
+ln -s /home/equivalence/base/src/ext/z3 src/ext/z3
+ln -s /home/equivalence/base/src/ext/cvc4-1.5-build src/ext/cvc4-1.5-build
+ln -s /home/equivalence/base/src/ext/cvc4-1.5 src/ext/cvc4-1.5
 
 # fix PATH
 echo "PATH=\"\$HOME/stoke/bin:\$PATH\"" >> ~/.bashrc
