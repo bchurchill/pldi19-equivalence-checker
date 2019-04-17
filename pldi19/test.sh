ded2a7908ee6732b18bb00535b97e202a62ec76e
Berkeley Churchill <berkc@stanford.edu>
2019-04-17 16:09:47 -0700

    adding debug output

--- pldi19/test.sh
+++ pldi19/test.sh
@@ -15,13 +15,19 @@ else
 	echo "EXAMPLE: failed"
 	FAILED=$((FAILED+1))
 fi
+echo "[debug] running make clean"
 make clean
+echo "[debug] running cd"
 cd ..
 
 ## TEST STRLEN
+echo "[debug] running cd 2"
 cd strlen
+echo "[debug] running make"
 make
+echo "[debug] running checker for strlen"
 time timeout 20m ./demo.sh | tee trace
+echo "[debug] grepping"
 grep "Equivalent: yes" trace
 if [ $? -eq "0" ]; then
 	echo "STRLEN: passed"
@@ -30,7 +36,9 @@ else
 	echo "STRLEN: failed"
 	FAILED=$((FAILED+1))
 fi
+echo "[debug] make clean"
 make clean
+echo "[debug] cd"
 cd ..
 
 echo "$SUCCESS tests passed, $FAILED tests failed"
