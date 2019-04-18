65cae3bf4fc8392284f22faa8007cc5669126478
Berkeley Churchill <berkc@stanford.edu>
2019-04-18 10:45:20 -0700

    removing strlen from test script

--- pldi19/test.sh
+++ pldi19/test.sh
@@ -3,50 +3,27 @@
 FAILED=0
 SUCCESS=0
 
-## TEST EXAMPLE
-cd paper_example
-make
-echo "[debug] disk space"
-df -h
-echo "[debug] running example"
-time timeout 10m ./demo.sh | tee trace
-echo "[debug] trace size"
-du -hs trace
-echo "[debug] grepping"
-grep "Equivalent: yes" trace
-if [ $? -eq "0" ]; then
-	echo "EXAMPLE: passed"
-	SUCCESS=$((SUCCESS+1))
-else
-	echo "EXAMPLE: failed"
-	FAILED=$((FAILED+1))
-fi
-echo "[debug] running make clean"
-make clean
-echo "[debug] running cd"
-cd ..
+function run_test () {
+  local FOLDER=$1
+  local NAME=$2
+  cd $FOLDER
+  echo "Running $NAME"
+  make > /dev/null
+  time timeout 10m ./demo.sh > trace
+  grep "Equivalent: yes" trace
+  if [ $? -eq "0" ]; then
+    echo "$NAME: passed"
+    SUCCESS=$((SUCCESS+1))
+  else
+    echo "$NAME: failed"
+    FAILED=$((FAILED+1))
+  fi
+  make clean >/dev/null
+  cd ..
 
-## TEST STRLEN
-echo "[debug] running cd 2"
-cd strlen
-echo "[debug] running make"
-make
-echo "[debug] running checker for strlen"
-time timeout 20m ./demo.sh | tee trace
-echo "[debug] grepping"
-grep "Equivalent: yes" trace
-if [ $? -eq "0" ]; then
-	echo "STRLEN: passed"
-	SUCCESS=$((SUCCESS+1))
-else
-	echo "STRLEN: failed"
-	FAILED=$((FAILED+1))
-fi
-echo "[debug] make clean"
-make clean
-echo "[debug] cd"
-cd ..
+}
 
+run_test "paper_example" "EXAMPLE"
 echo "$SUCCESS tests passed, $FAILED tests failed"
 
 exit $FAILED
